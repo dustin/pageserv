a45fd4a0e346f8207cc89b3c40257f299ffa02a9
Dustin Sallings <dustin@spy.net>
1997-03-11 13:58:13 +0000

    Does banner thing, calls process function, no more output.
    Author: dustin
    Date: 1997/03/11 05:58:13

--- kids.c
+++ kids.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1997  Dustin Sallings
  *
- * $Id: kids.c,v 1.1 1997/03/10 07:34:38 dustin Exp $
+ * $Id: kids.c,v 1.2 1997/03/11 05:58:13 dustin Exp $
  */
 
 #include <signal.h>
@@ -27,27 +27,27 @@ void onalarm()
     exit(0);
 }
 
-void process(int s)
+void childmain(int s)
 {
     char buf[BUFLEN];
     int size;
+
+    sprintf(buf, "Welcome to Dustin's pager server version %s.\n", VERSION);
+    send(s, buf, strlen(buf), 0);
 
     /* lifetime of 120 seconds */
     alarm(CHILD_LIFETIME);
     signal(SIGALRM, onalarm);
 
-    for(;;)
+    if( (size=recv(s, buf, BUFLEN-1, 0)) >0 )
     {
-         if( (size=recv(s, buf, BUFLEN-1, 0)) >0 )
-         {
-             buf[size]=0x00;
-             kw(buf);
-             printf("Got ``%s'' (%d bytes)\n", buf, size);
-         }
-	 else
-	 {
-	     /* If this isn't a pipe, I don't know what the hell... */
-	     exit(0);
-	 }
+         buf[size]=0x00;
+         kw(buf);
+	 process(s, buf);
+    }
+    else
+    {
+         /* If this isn't a pipe, I don't know what the hell... */
+         exit(0);
     }
 }
