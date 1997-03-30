89d43af626059fa2cc19cb6179390be36eb59b2a
Dustin Sallings <dustin@spy.net>
1997-03-30 09:35:40 +0000

    Oops, forgot to add this, the taptest.
    Author: dustin
    Date: 1997/03/30 01:35:40
        Oops, forgot to add this, the taptest.

---
+++ delivery/taptest.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 1997  Dustin Sallings
+ *
+ * $Id: taptest.c,v 1.1 1997/03/30 01:35:40 dustin Exp $
+ */
+
+#include <stdio.h>
+
+#include "tap.h"
+#include "../pageserv.h"
+
+struct config conf;
+
+void main(void)
+{
+    int s;
+
+    s=openhost("liv-lex-1.ipa.net.", 6001);
+    s_modem_connect(s, "9,783.6426");
+    puts("I got connected.  :)");
+    s_tap_init(s);
+
+    if( s_tap_send(s, "7888802", "This is a TAP test.") == 0)
+	puts(":) Message was successful");
+    else
+	puts(":( Message was unsuccessful");
+
+    if( s_tap_send(s, "7888802", "This is another TAP test.") == 0)
+	puts(":) Message was successful");
+    else
+	puts(":( Message was unsuccessful");
+
+    s_tap_end(s);
+    puts("Disco-necting");
+    close(s);
+
+    sleep(5);
+
+/*
+    s=openhost("liv-lex-1.ipa.net.", 6001);
+    s_modem_connect(s, "9,1,501.219.1805");
+    puts("I got connected.  :)");
+    s_tap_init(s);
+
+    if( s_tap_send(s, "3123362", "This is a TAP test.") == 0)
+	puts(":) Message was successful");
+    else
+	puts(":( Message was unsuccessful");
+
+    if( s_tap_send(s, "3123362", "This is another TAP test.") == 0)
+	puts(":) Message was successful");
+    else
+	puts(":( Message was unsuccessful");
+
+    s_tap_end(s);
+    puts("Disco-necting");
+    close(s);
+*/
+}
