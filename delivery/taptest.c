53460d242f0f03fc43219c23a04c1d8aa3cdad00
Dustin Sallings <dustin@spy.net>
1997-10-23 00:38:25 +0000

    Send enough flags along with tap_init
    Author: dustin
    Date: 1997/10/22 16:38:25
        Send enough flags along with tap_init

--- delivery/taptest.c
+++ delivery/taptest.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1997  Dustin Sallings
  *
- * $Id: taptest.c,v 2.5 1997/06/19 08:24:36 dustin Exp $
+ * $Id: taptest.c,v 2.6 1997/10/22 16:38:25 dustin Exp $
  */
 
 #include <stdio.h>
@@ -21,7 +21,7 @@ void main(void)
     s=s_openhost("liv-lex-1.ipa.net.", 6001);
     s_modem_connect(s, "9,783.6426");
     puts("I got connected.  :)");
-    s_tap_init(s);
+    s_tap_init(s, 1);
 
     if( s_tap_send(s, "7888802", "This is a TAP test, please ignore") == 0)
 	puts(":) Message was successful");
@@ -42,18 +42,18 @@ void main(void)
 
     s=p_openport("/dev/ttyb");
     puts("Whew, made it back, let's try to connect.");
-    s_modem_connect(s, "1.510.801.9999");
+    s_modem_connect(s, "1.800.455.2698");
     puts("I got connected.  :)");
-    s_tap_init(s);
+    s_tap_init(s, 1);
 
-    if( s_tap_send(s, "2534645", "This is a TAP test.") == 0)
+    if( s_tap_send(s, "1106710", "This is a TAP test.") == 0)
 	puts(":) Message was successful");
     else
 	puts(":( Message was unsuccessful");
 
     usleep(2600);
 
-    if( s_tap_send(s, "2534645", "This is another TAP test.") == 0)
+    if( s_tap_send(s, "1106710", "This is another TAP test.") == 0)
 	puts(":) Message was successful");
     else
 	puts(":( Message was unsuccessful");
