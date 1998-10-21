88cac9dbccc08ce472dbb819aa54cf63f3349080
Dustin Sallings <dustin@spy.net>
1998-10-21 08:34:23 +0000

    New delivery stuff from ntmartin@ipa.net
    Author: dustin
    Date: 1998/10/21 00:34:23

--- delivery/misc.c
+++ delivery/misc.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1997  Dustin Sallings
  *
- * $Id: misc.c,v 2.7 1998/07/15 07:54:47 dustin Exp $
+ * $Id: misc.c,v 2.8 1998/10/21 00:34:23 dustin Exp $
  */
 
 #include <stdio.h>
@@ -23,34 +23,35 @@
 
 extern struct config conf;
 
-void cleanmylocks()
+void
+cleanmylocks()
 {
-    DIR *dir;
-    FILE *f;
-    struct dirent *d;
-    int i=0, pid;
+	DIR    *dir;
+	FILE   *f;
+	struct dirent *d;
+	int     i = 0, pid;
 
-    pid=getpid();
+	pid = getpid();
 
-    chdir(conf.qdir);
-    dir=opendir(".");
+	chdir(conf.qdir);
+	dir = opendir(".");
 
-    while( (d=readdir(dir))!=NULL) {
-        if(d->d_name[0]=='l') {
-            f=fopen(d->d_name, "r");
-            if(f!=NULL) {
-                fscanf(f, "%d", &i);
-                if(i==pid) {
-                    _ndebug(0, ("Getting rid of %s\n", d->d_name));
-                    unlink(d->d_name);
-                }
-                fclose(f);
-            }
-        }
-    }
-    closedir(dir);
+	while ((d = readdir(dir)) != NULL) {
+		if (d->d_name[0] == 'l') {
+			f = fopen(d->d_name, "r");
+			if (f != NULL) {
+				fscanf(f, "%d", &i);
+				if (i == pid) {
+					_ndebug(0, ("Getting rid of %s\n", d->d_name));
+					unlink(d->d_name);
+				}
+				fclose(f);
+			}
+		}
+	}
+	closedir(dir);
 
-    checklocks(); /* this is for serial locks */
+	checklocks();		/* this is for serial locks */
 }
 
 /*
@@ -58,120 +59,152 @@ void cleanmylocks()
  * the way it works without relinking everything.
  */
 
-void del_log(char *format, ...)
+void
+del_log(char *format,...)
 {
-    va_list ap;
-    char buf[BUFLEN];
+	va_list ap;
+	char    buf[BUFLEN];
 
-    openlog("pageserv", LOG_PID|LOG_NDELAY, conf.log_que);
+	openlog("pageserv", LOG_PID | LOG_NDELAY, conf.log_que);
 
-    va_start(ap, format);
-    vsnprintf(buf, BUFLEN-1, format, ap);
-    va_end(ap);
+	va_start(ap, format);
+	vsnprintf(buf, BUFLEN - 1, format, ap);
+	va_end(ap);
 
-    syslog(conf.log_que|LOG_INFO, buf);
+	syslog(conf.log_que | LOG_INFO, buf);
 
-    closelog();
+	closelog();
 }
 
-void runqueue(void)
+void
+runqueue(void)
 {
-    struct queuent *q;
-    char **termlist;
-    struct terminal term;
-    int t, i, s, r;
+	struct queuent *q;
+	char  **termlist;
+	struct terminal term;
+	int     t, i, s, r;
 
-    conf.udb.dbinit();
-    resetdelivertraps();
-    termlist=listterms();
+	conf.udb.dbinit();
+	resetdelivertraps();
+	termlist = listterms();
 
-    /* four minutes to complete delivery */
-    alarm(240);
-    for(t=0; termlist[t]!=NULL; t++) {
-        _ndebug(0, ("Queue for %s:\n", termlist[t]));
-        q=listqueue(termlist[t]);
+	/* two minutes to complete delivery */
+	alarm(120);
 
-        for(i=0; q[i].to[0] != NULL; i++);
+	for (t = 0; termlist[t] != NULL; t++) {
+		_ndebug(0, ("Queue for %s:\n", termlist[t]));
+		q = listqueue(termlist[t]);
 
-        if(i>0) {
-            term=getterm(termlist[t]);
+		for (i = 0; q[i].to[0] != NULL; i++);
 
-            if( (s=any_openterm(term))>=0) {
-                /* Inside this if is executed if I got the port */
+		if (i > 0) {
+			del_log("starting to process queue %d", i);
+			term = getterm(termlist[t]);
 
-                if(s_tap_init(s, term.flags)<0) {
-		    any_closeterm(s, term);
-		    sleep(5); /* sleep it off */
-		    break;
-                }
+			if ((s = any_openterm(term)) >= 0) {
+				/* Inside this if is executed if I got the port */
 
-                /* Keep looping through queue stuff until there are no more
-                   requests, in case any where queued while we're already
-                   delivering.  */
-                while(q[0].to[0] != NULL) {
+				if (s_tap_init(s, term.flags) < 0) {
+					any_closeterm(s, term);
+					sleep(5);	/* sleep it off */
+					break;
+				}
+				/* Keep looping through queue stuff until there are no more
+				 * requests, in case any where queued while we're already
+				 * delivering.  */
+				while (q[0].to[0] != NULL) {
+					/* too debuggy */
+					/* del_log("start of while loop"); */
 
-                    for(i=0; q[i].to[0] != NULL; i++) {
-                        getqueueinfo(&q[i]);
+					for (i = 0; q[i].to[0] != NULL; i++) {
+						getqueueinfo(&q[i]);
 
-                        if( (r=s_tap_send(s, q[i].u.pageid, q[i].message))==0){
-                            _ndebug(0, ("Delivery of %s successful\n",
-                                q[i].qid));
-			    del_log("delivered %s to %s: %d bytes",
-				    q[i].qid, q[i].to, strlen(q[i].message));
-			    dequeue(q[i].qid);
-			    usleep(2600);
-                        } else {
-			    if(r==C_RS) {
-				dq_notify(q[i], "Unsuccessful", NOTIFY_FAIL);
-                                del_log("failed %s to %s, got RS (fatal)",
-					q[i].qid, q[i].to);
-				dequeue(q[i].qid);
-			    } else {
-                                del_log("deferred %s to %s, "
-                                        "will keep trying until it expires",
-					q[i].qid, q[i].to);
-                                q_unlock(q[i]);
-			    }
-                        }
-                        _ndebug(2, ("\t%d to %s  ``%s''\n", i, q[i].to,
-                            q[i].message));
-                    } /* for loop */
+						/* Too debuggy */
+						/* del_log("attempt s_tap_send mess: %s",
+						   q[i].message); */
+						r = s_tap_send(s, q[i].u.pageid, q[i].message);
 
-                    cleanqueuelist(q);
-                    q=listqueue(termlist[t]);
+						if (r == 0) {
+							_ndebug(0, ("Delivery of %s successful\n",
+								q[i].qid));
+							del_log("delivered %s to %s: %d bytes",
+							    q[i].qid, q[i].to, strlen(q[i].message));
+							dequeue(q[i].qid);
+							usleep(2600);
+						} else if (r == 'X' && i == 0) {
+							_ndebug(0, ("Delivery of %s probably successful\n",
+								q[i].qid));
+							del_log("probably delivered %s to %s: %d bytes",
+							    q[i].qid, q[i].to, strlen(q[i].message));
+							dequeue(q[i].qid);
+							usleep(2600);
+							/* Must re-connect before sending any more. */
+							break;
+						} else if (r == C_RS) {
+							dq_notify(q[i], "Unsuccessful", NOTIFY_FAIL);
+							del_log("failed %s to %s, got RS (fatal)",
+							    q[i].qid, q[i].to);
+							dequeue(q[i].qid);
+						} else {
+							del_log("deferred %s to %s, "
+							    "will keep trying until it expires",
+							    q[i].qid, q[i].to);
+							q_unlock(q[i]);
+							/* Must re-connect. */
+							if (r == 'X') {
+								break;
+							};
+						}
+						_ndebug(2, ("\t%d to %s  ``%s''\n", i, q[i].to,
+							q[i].message));
+					}	/* for loop */
 
-                } /* while loop */
+					cleanqueuelist(q);
 
-                s_tap_end(s);
-                any_closeterm(s, term);
-                sleep(5); /* sleep it off */
-            } else {
-		del_log("Failed to get a port");
-                _ndebug(2, ("Didn't get the port\n"));
-            }
-        }
+					/* Must re-connect. */
+					if (r == 'X') {
+						break;
+					};
 
-        cleanqueuelist(q);
+					q = listqueue(termlist[t]);
 
-    }
-    cleantermlist(termlist);
-    cleanmylocks();
+				}	/* while loop */
+
+				/* Too debuggy */
+				/* del_log("attempting s_tap_end"); */
+				s_tap_end(s);
+				any_closeterm(s, term);
+				sleep(5);	/* sleep it off */
+			} else {
+				del_log("Failed to get a port");
+				_ndebug(2, ("Didn't get the port\n"));
+			}
+		}
+		cleanqueuelist(q);
+
+	}
+	cleantermlist(termlist);
+	cleanmylocks();
+	/* Too debuggy */
+	/* del_log("end of runqueue"); */
 }
 
-static RETSIGTYPE del_sigint(int sig)
+static RETSIGTYPE
+del_sigint(int sig)
 {
-    cleanmylocks();
-    del_log("Caught signal %d, exiting...", sig);
-    exit(1);
+	cleanmylocks();
+	del_log("Caught signal %d, exiting...", sig);
+	exit(1);
 }
 
-void resetdelivertraps(void)
+void
+resetdelivertraps(void)
 {
-    _ndebug(0, ("Setting signals...\n"));
+	_ndebug(0, ("Setting signals...\n"));
 
-    signal(SIGINT, del_sigint);
-    signal(SIGQUIT, del_sigint);
-    signal(SIGTERM, del_sigint);
-    signal(SIGHUP, del_sigint);
-    signal(SIGALRM, del_sigint);
+	signal(SIGINT, del_sigint);
+	signal(SIGQUIT, del_sigint);
+	signal(SIGTERM, del_sigint);
+	signal(SIGHUP, del_sigint);
+	signal(SIGALRM, del_sigint);
 }
--- delivery/tap.c
+++ delivery/tap.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1997  SPY Internetworking
  *
- * $Id: tap.c,v 2.25 1998/07/15 07:54:55 dustin Exp $
+ * $Id: tap.c,v 2.26 1998/10/21 00:34:24 dustin Exp $
  */
 
 #include <stdio.h>
@@ -9,6 +9,9 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+
+#include <stropts.h>
+#include <sys/conf.h>
 
 #include <pageserv.h>
 #include <tap.h>
@@ -18,147 +21,171 @@ extern struct config conf;
 static int tap_checksum(char *string);
 static char *tap_sent_checksum(int sum);
 
-static int tap_checksum(char *string)
+static int
+tap_checksum(char *string)
 {
-    int i, sum=0;
+	int     i, sum = 0;
 
-    for(i=0; string[i]!=0; i++) {
-	sum+=string[i] - ( (int)(string[i]/128) * 128);
-    }
-
-    return(sum);
-}
-
-static char *tap_sent_checksum(int sum)
-{
-    static char charsum[4]={0,0,0,0};
-
-    charsum[2]=48+ sum - (int)(sum/16) * 16;
-    sum = (int)(sum/16);
-    charsum[1]=48+ sum - (int)(sum/16) * 16;
-    sum = (int)(sum/16);
-    charsum[0]=48+ sum - (int)(sum/16) * 16;
-
-    return(charsum);
-}
-
-int s_tap_init(int s, int flags)
-{
-    char buf[BUFLEN];
-    int retry, ok;
-
-    _ndebug(0, ("Initializing TAP\n"));
-
-    if(flags & TAP_INITDELAY) {
-	_ndebug(3, ("flag TAP_INITDELAY is set, sleeping\n"));
-
-	sleep(1);
-    }
-
-    /* we won't do any of this if flag TAP_INITCR isn't set */
-    if(flags & TAP_INITCR) {
-
-	_ndebug(3, ("flag TAP_INITCR is set, doing CR dance\n"));
-        usleep(500000); /* Just to make sure everything's ready */
-
-	ok=0;
-	for(retry=0; retry<RETRY_1; retry++) {
-	    puttext(s, "\r");
-
-            if( s_modem_waitfor(s, "ID=", TIME_2) < 0) {
-	      _ndebug(2, ("\t...timed out, sending another CR\n"));
-              del_log("Timed out waiting for ID, retry #%d", retry);
-            } else {
-	      ok=1; break;
-	    }
+	for (i = 0; string[i] != 0; i++) {
+		sum += string[i] - ((int) (string[i] / 128) * 128);
 	}
 
-	if(!ok) {
-	    del_log("Too many init timeouts");
-            return(-1);
-        }
-    }
-
-    sprintf(buf, "%cPG1\r", C_ESC);
-    puttext(s, buf);
-
-    sprintf(buf, "%c[p", C_ESC);
-    if( s_modem_waitfor(s, buf, TIME_3) < 0) {
-	_ndebug(2,("Error:  s_modem_waitfor(%d, \"%s\", %d);\n",s,buf,TIME_3));
-        del_log("timed out waiting for TAP init reply");
-	return(-1);
-    }
-    return(0);
+	return (sum);
 }
 
-int s_tap_end(int s)
+static char *
+tap_sent_checksum(int sum)
 {
-    char buf[BUFLEN];
+	static char charsum[4] =
+	{0, 0, 0, 0};
 
-    _ndebug(0, ("ending TAP session\n"));
+	charsum[2] = 48 + sum - (int) (sum / 16) * 16;
+	sum = (int) (sum / 16);
+	charsum[1] = 48 + sum - (int) (sum / 16) * 16;
+	sum = (int) (sum / 16);
+	charsum[0] = 48 + sum - (int) (sum / 16) * 16;
 
-    sprintf(buf, "%c\r", C_EOT);
-    puttext(s, buf);
-    return(0);
+	return (charsum);
 }
 
-static int charfound(char *string, char *chars)
+int
+s_tap_init(int s, int flags)
 {
-    int i;
+	char    buf[BUFLEN];
+	int     retry, ok;
 
-    for(i=0; chars[i]!=0x00; i++) {
-	if(strchr(string, chars[i])!=NULL) {
-	    return(chars[i]);
+	_ndebug(0, ("Initializing TAP\n"));
+
+	if (flags & TAP_INITDELAY) {
+		_ndebug(3, ("flag TAP_INITDELAY is set, sleeping\n"));
+
+		sleep(1);
 	}
-    }
-    return(0);
+	/* we won't do any of this if flag TAP_INITCR isn't set */
+	if (flags & TAP_INITCR) {
+
+		_ndebug(3, ("flag TAP_INITCR is set, doing CR dance\n"));
+		usleep(500000);	/* Just to make sure everything's ready */
+
+		ok = 0;
+		for (retry = 0; retry < RETRY_1; retry++) {
+			puttext(s, "\r");
+
+			if (s_modem_waitfor(s, "ID=", TIME_2) < 0) {
+				_ndebug(2, ("\t...timed out, sending another CR\n"));
+				del_log("Timed out waiting for ID, retry #%d", retry);
+			} else {
+				ok = 1;
+				break;
+			}
+		}
+
+		if (!ok) {
+			del_log("Too many init timeouts");
+			return (-1);
+		}
+	}
+	sprintf(buf, "%cPG1\r", C_ESC);
+	puttext(s, buf);
+
+	sprintf(buf, "%c[p", C_ESC);
+	if (s_modem_waitfor(s, buf, TIME_3) < 0) {
+		_ndebug(2, ("Error:  s_modem_waitfor(%d, \"%s\", %d);\n", s, buf, TIME_3));
+		del_log("timed out waiting for TAP init reply");
+		return (-1);
+	}
+	return (0);
 }
 
-int s_tap_send(int s, char *id, char *message)
+int
+s_tap_end(int s)
 {
-    char buf[BUFLEN];
-    char search[]={C_ACK, C_ESC, C_NAK, C_RS, 0x00};
-    char c;
-    int i;
+	char    buf[BUFLEN];
 
-    _ndebug(0, ("Sending message to %s:\n%s\n", id, message));
+	_ndebug(0, ("ending TAP session\n"));
 
-    /* cheap bounds checking */
-    if(strlen(message)+strlen(id)>(BUFLEN-20)) {
-        del_log("ACK!  Length of message+id is too long (%s:%d)!",
-		__FILE__, __LINE__);
-	return(-1);
-    }
+	sprintf(buf, "%c\r", C_EOT);
+	puttext(s, buf);
+	return (0);
+}
 
-    sprintf(buf, "%c%s%c%s%c%c", C_STX, id, C_CR, message, C_CR, C_ETX);
+static int
+charfound(char *string, char *chars)
+{
+	int     i;
 
-    /* Stick the checksum on the end */
-    strcat(buf, do_checksum(buf));
-    strcat(buf, "\r");
-
-    /* It compiles, ship it. */
-    puttext(s, buf);
-
-    /* Lets see if we won. */
-
-    c=0;
-    strcpy(buf, ""); /* clean out buf */
-    while( (c=charfound(buf, search)) ==0 ) {
-	i=read(s, buf, BUFLEN);
-	if(i>0) {
-	    buf[i]=0x00;
-	    _ndebug(2, ("%s\n", buf));
+	for (i = 0; chars[i] != 0x00; i++) {
+		if (strchr(string, chars[i]) != NULL) {
+			return (chars[i]);
+		}
 	}
-    }
+	return (0);
+}
 
-    /* Another random sleepy thing, just to get the timing right */
-    usleep(1700);
+int
+s_tap_send(int s, char *id, char *message)
+{
+	char    buf[BUFLEN];
+	char    search[] =
+	{C_ACK, C_ESC, C_NAK, C_RS, 0x00};
+	char    c;
+	int     i;
 
-    /* I used to do escape here...  hmm...  */
-    if(c==C_ACK) {
-	return(0);
-    } else {
-	_ndebug(2, (":( Received character %xh\n", c));
-	return((int)c);
-    }
+	_ndebug(0, ("Sending message to %s:\n%s\n", id, message));
+
+	/* cheap bounds checking */
+	if (strlen(message) + strlen(id) > (BUFLEN - 20)) {
+		del_log("ACK!  Length of message+id is too long (%s:%d)!",
+		    __FILE__, __LINE__);
+		return (-1);
+	}
+	/* Probably this is important. */
+	ioctl(s, I_FLUSH, FLUSHR);
+
+	sprintf(buf, "%c%s%c%s%c%c", C_STX, id, C_CR, message, C_CR, C_ETX);
+
+	/* Stick the checksum on the end */
+	strcat(buf, do_checksum(buf));
+	strcat(buf, "\r");
+
+	/* It compiles, ship it. */
+	puttext(s, buf);
+
+	/* Lets see if we won. */
+
+	c = 0;
+	strcpy(buf, "");	/* clean out buf */
+	while ((c = charfound(buf, search)) == 0) {
+		_ndebug(2, ("attempting read from s\n"));
+		i = read(s, buf, 1);
+		if (i > 0) {
+			buf[i] = 0x00;
+			_ndebug(2, ("%s\n", buf));
+		} else {
+			/* Over so soon? */
+			_ndebug(2, ("nothing returned that we like\n", buf));
+			/* X == EOF */
+			c = 'X';
+			break;
+		}
+	}
+
+	/* This may not be quite as important. */
+	ioctl(s, I_FLUSH, FLUSHR);
+
+	/* Another random sleepy thing, just to get the timing right */
+	usleep(1700);
+
+	/* I used to do escape here...  hmm...  */
+	if (c == C_ACK) {
+		_ndebug(2, ("Received character %xh.  Good.\n", c));
+		/* Too debuggy */
+		/* del_log("Received character %xh.  Good.\n", c); */
+		return (0);
+	} else {
+		_ndebug(2, (":( Received character %xh\n", c));
+		/* Too debuggy */
+		/* del_log(":( Received character %xh\n", c); */
+		return ((int) c);
+	}
 }
